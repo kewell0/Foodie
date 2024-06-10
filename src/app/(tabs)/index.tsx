3cf62c469aeef80c7d4d4a0b9d1e598f2670693d
Caleb Owatah <owatahcaleb@gmail.com>
2024-06-10 09:42:00 +0100

    add custom component

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -2,7 +2,7 @@ import { ScrollView } from "react-native";
 
 // import { Text, View } from "../../components/Themed";
 
-import products from "../../../assets/data/products";
+import products from "@assets/data/products";
 import ProductListItem from "@/src/components/ProductListItem";
 
 export default function MenuScreen() {
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -2,21 +2,22 @@ import { Text, View, Image, StyleSheet } from "react-native";
 
 // import { Text, View } from "../../components/Themed";
 import Colors from "@/src/constants/Colors";
+import { Product } from "../types";
 
-interface Product {
-  name: string;
-  price: number;
-  image: string;
-}
+export const defaultPizzaImage =
+  "'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png'";
 
 interface ProductListItemProps {
   product: Product;
 }
 
-const ProductListItem: React.FC<ProductListItemProps> = ({ product }) => {
+const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
     <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image} />
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+      />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
     </View>
