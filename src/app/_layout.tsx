0f6515f16992ff2c31685daeff48ca51557a7d75
Caleb Owatah <owatahcaleb@gmail.com>
2024-06-13 02:25:45 +0100

    feat: add users

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -57,7 +57,7 @@ function RootLayoutNav() {
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <CartProvider>
         <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="(user)" options={{ headerShown: false }} />
           <Stack.Screen
             name="cart"
             options={{
